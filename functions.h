26c9ad159297a8f0a496c2bd4ae7f5498992353b
anushakartala <57739547+anushakartala@users.noreply.github.com>
2020-01-23 10:04:41 -0600

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,10 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+
+void printUsageInfo(char programName[]);
+bool isPalindromeR(char inputString[]);
+bool isPalindrome(char inputString[], bool ignoreCase, bool ignoreSpace);
+
+
+#endif
